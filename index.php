e026bcfde86212ea0ddd9f28a85588784efb2697
Sara <saracorsob@gmail.com>
2020-07-27 16:23:56 +0200

    Scrittura css

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
   <head>
     <meta charset="utf-8">
     <title>Boolean Hotel</title>
+
+    <link rel="stylesheet" href="dist/app.css">
   </head>
   <body>
 
@@ -11,7 +13,31 @@
     </header>
 
     <main>
-      
+      <table>
+        <thead>
+          <th>ID</th>
+          <th>Room Number</th>
+          <th>Floor</th>
+        </thead>
+
+        <tbody>
+          <td>1</td>
+          <td>101</td>
+          <td>3</td>
+        </tbody>
+
+        <tbody>
+          <td>2</td>
+          <td>102</td>
+          <td>3</td>
+        </tbody>
+
+        <tbody>
+          <td>3</td>
+          <td>104</td>
+          <td>5</td>
+        </tbody>
+      </table>
     </main>
 
   </body>
