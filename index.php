a06e6193416a9c6ae874bbb27c84e54d17a8cc76
Sara <saracorsob@gmail.com>
2020-07-27 16:00:35 +0200

    Aggiunta file tranne database

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Boolean Hotel</title>
+  </head>
+  <body>
+
+    <header>
+      <h1>Boolean Hotel</h1>
+    </header>
+
+    <main>
+      
+    </main>
+
+  </body>
+</html>
